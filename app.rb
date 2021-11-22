b9084a328a191db091a39f9301da91ea923e21a0
Corban <sonnemanc@gmail.com>
2021-11-21 18:08:58 -0700

    commit tree 356eaedea5c715be14089499d02ce639534302d6

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        iglatinpay = PigLatinizer.new
+        @piglatin = iglatinpay.piglatinize(params[:user_phrase])
+        erb :results
+    end 
 end
